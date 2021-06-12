00672e4aad1358d4f0b0ce796ed0f20069e760d3
backend-1stdev <backend@1stdev.ru>
2021-06-13 00:40:36 +0300

    Initial commit

---
+++ BroBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BroBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
